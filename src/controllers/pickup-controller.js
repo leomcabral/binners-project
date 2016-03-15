d3ebc18b472ba1a9605a15260372702cec696755
Leonardo de M. Cabral <leomcabral@gmail.com>
2016-03-15 01:01:43 -0300

    File upload implementation for pickups. When a pickup is placed an url is available in the response. This URL can be used to upload files to the created pickup.

--- src/controllers/pickup-controller.js
+++ src/controllers/pickup-controller.js
@@ -14,7 +14,13 @@ var Boom = require('boom'),
         config = require('config'),
         models = require('../models'),
         errors = require('../lib/utilities').getErrorsCode(),
-        moment = require('moment-timezone');
+        Promise = require('bluebird'),
+        fs = Promise.promisifyAll(require('fs')),
+        moment = require('moment-timezone'),
+        inspect = require('eyes').inspector({styles: {all: 'green'}}),
+        uuid = require('node-uuid');
+
+var pickupFolder = config.get("SERVER.UPLOAD_FOLDER") + "/pickups/";
 
 function PickupController() {}
 
@@ -23,9 +29,9 @@ PickupController.prototype = (function () {
     var Pickup = models.Pickup;
 
     return {
-
         pickup: function (request, reply) {
             var payload = request.payload;
+            var userId = request.auth.credentials.user;
 
             var newPickup = {
                 requester: payload.requester,
@@ -37,7 +43,11 @@ PickupController.prototype = (function () {
 
             Pickup.create(newPickup).then(function (pickup) {
 
-                reply(pickup);
+                var ret = pickup.toObject();
+                var sInfo = request.server.info;
+                ret.photoUploadURL = sInfo.protocol + '://' + sInfo.host + ':'+ sInfo.port + '/api/' + config.get('SERVER.API_VERSION') + '/pickups/' + userId + '/photos';
+
+                reply(ret);
 
             }, function (error) {
                 reply(Boom.badRequest(error));
@@ -47,15 +57,79 @@ PickupController.prototype = (function () {
         list: function (request, reply) {
             var curDate = moment().tz('America/Vancouver');
             var last6Month = curDate.clone().subtract(6, 'months');
+            var userId = request.auth.credentials.user;
 
-            Pickup.find({ time: {"$gte": last6Month} }).exec()
+            Pickup.find({requester: userId, time: {"$gte": last6Month}}).exec()
                     .then(function (pickups) {
                         reply(pickups);
                     });
+        },
+        photoUpload: function (request, reply) {
+            var userId = request.auth.credentials.user;
+
+            var data = request.payload;
+            data.userId = userId;
+
+            Promise.resolve(data)
+                    .then(createFolderForUserIfNotPresent)
+                    .then(uploadFile)
+                    .then(function (data) {
+                        var sInfo = request.server.info;
+
+                        reply({
+                            status: '200',
+                            msg: 'File uploaded succefully',
+                            fileURL: sInfo.protocol + '://' + sInfo.host + ':'+ sInfo.port + '/api/' + config.get('SERVER.API_VERSION') + '/pickups/' + data.userId + '/photos/' + data.generatedFilename
+                        });
+                    })
+                    .catch(function (err) {
+                        reply(Boom.badRequest(err));
+                    });
+
         }
 
     };
 
 })();
+
+var createFolderForUserIfNotPresent = function (data) {
+    var userPickupFolder = pickupFolder + '/' + data.userId;
+    data.userPickupFolder = userPickupFolder;
+
+    return new Promise(function (resolve, reject) {
+        fs.stat(userPickupFolder, function (err, stat) {
+            if (err) {
+                inspect('[ Pickup ] Folder "' + userPickupFolder + '" not found. It will be created!');
+                fs.mkdirSync(userPickupFolder);
+                resolve(data);
+            } else {
+                inspect('[ Pickup ] Folder "' + userPickupFolder + '" already exists');
+                resolve(data);
+            }
+        });
+    });
+};
+
+var uploadFile = function (data) {
+    var requestFilename = data.file.hapi.filename;
+    var filename = uuid.v4 + '.' + requestFilename.substring(requestFilename.lastIndexOf('.') + 1, requestFilename.length);
+    var filePath = data.userPickupFolder + '/' + filename;
+    data.filePath = filePath;
+    data.generatedFilename = filename;
+
+    return new Promise(function (resolve, reject) {
+        var file = fs.createWriteStream(filePath);
+
+        file.on('error', function (err) {
+            reject(err);
+        });
+
+        data.file.pipe(file);
+
+        data.file.on('end', function (err) {
+            resolve(data);
+        });
+    });
+};
 
 module.exports = new PickupController();
